80779c56cbece49ff43ae6348915e197a6cf1167
zhaobing <CreamBing1994@gmail.com>
2016-12-15 10:43:31 +0800

    项目结构调整

---
+++ src/main/java/com/bing/hdfsV/HdfsViewApp.java
@@ -0,0 +1,21 @@
+package com.bing.hdfsV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * main主程序
+ * Created by zhaobing on 2016/12/15.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.bing.hdfsV.conf"})
+public class HdfsViewApp {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(HdfsViewApp.class);
+        application.run(args);
+    }
+}
---
+++ src/main/java/com/bing/hdfsV/conf/ApplicationConfiguration.java
@@ -0,0 +1,13 @@
+package com.bing.hdfsV.conf;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * springboot 扫描配置
+ * Created by zhaobing on 2016/12/15.
+ */
+@Configuration
+@ComponentScan(basePackages={"com.bing.hdfsV.web","com.bing.hdfsV.service"})
+public class ApplicationConfiguration {
+}
